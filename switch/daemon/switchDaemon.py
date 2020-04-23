3a26b46df0fbec6dcbf1b08cd2edd2bf356ad921
phar <phar@lil-Euclid-10.local>
2020-04-23 11:18:48 -0700

    adding file`

---
+++ switch/daemon/switchDaemon.py
@@ -0,0 +1,50 @@
+import daemon
+from r00tzgpio import *
+from util import *
+import time
+from util import *
+
+def resetbtn_event():
+	pass
+	
+def switchbtn_event():
+	if existsfileFile("r00tzSwitchOn"):
+		clearFile("r00tzSwitchOn")
+	else:
+		touchFile("r00tzSwitchOn")
+
+CHECK_SWITCH_INTERVAL = 60
+CHECK_SWITCH_LAST_TIME = 0
+CHECK_UPDATE_INTERVAL = (60 * 60) * 15
+CHECK_UPDATE_LAST_TIME = 0
+
+def main_program():
+	gapi = r00tsIoTGPIO(switchpress=switchbtn_eventm resetpress=resetbtn_event, logfunc=logevent)
+    while True:
+		if existsFile(r00tzSwitchOn):
+			gapi.relay_on()
+		else:
+			gapi.relay_off()
+			
+		if existsfileFile("r00tzRegistered"):
+			if (CHECK_SWITCH_LAST_TIME + CHECK_UPDATE_INTERVAL) < time.time()
+				ret = rapi.apiGetStatus(getFile("r00tzSwitchID"))
+				if ret["status"][1] == "ON":
+					gapi.led_on("relay_led")
+					gapi.relay_on()
+				else:
+					gapi.led_off("relay_led")
+					gapi.relay_off()
+				CHECK_SWITCH_LAST_TIME = time.time()
+
+		if (CHECK_UPDATE_LAST_TIME + CHECK_UPDATE_INTERVAL) < time.time()
+			ret = rapi.apiCheckUpdate()
+			#fixme do update
+			CHECK_UPDATE_LAST_TIME = time.time()
+
+			
+		time.sleep(.25)
+
+with daemon.DaemonContext():
+    main_program()
+ 
--- switch/r00tzgpio.py
+++ switch/r00tzgpio.py
@@ -29,138 +29,274 @@ try:
 	BUTTON_PRESSED_STATE =  0
 	BUTTON_RELEASED_STATE = 1
 
-	class r00tsIoTGPIO():
-		def __init__(self, switchpress=lambda: None,resetpress=lambda: None, logfunc=lambda: None):
-			self.leds = LEDMAP
-			self.buttons =BUTTONMAP
-			self.logger = logfunc
-			self.switchpress = switchpress
-			GPIO.setmode(GPIO.BCM)
-			GPIO.setwarnings(False)
+#	class r00tsIoTGPIO():
+#		def __init__(self, switchpress=lambda: None,resetpress=lambda: None, logfunc=lambda: None):
+#			self.leds = LEDMAP
+#			self.buttons =BUTTONMAP
+#			self.logger = logfunc
+#			self.switchpress = switchpress
+#			GPIO.setmode(GPIO.BCM)
+#			GPIO.setwarnings(False)
+#
+#			for n,l in self.leds.items():
+#				GPIO.setup(l, GPIO.OUT)
+#
+#			for n,b in self.buttons.items():
+#				GPIO.setup(b, GPIO.IN)
+#
+#			GPIO.setup(IOT_SWITCH_PIN, GPIO.OUT)
+#
+#			if BUTTON_PRESSED_STATE == 0:
+#				GPIO.add_event_detect(INPUT_BUTTON_0_PIN, GPIO.FALLING, callback = self.switchpress, bouncetime = 100)
+#				GPIO.add_event_detect(INPUT_BUTTON_1_PIN, GPIO.FALLING, callback = self.resetpress, bouncetime = 100)
+#			else:
+#				GPIO.add_event_detect(INPUT_BUTTON_0_PIN, GPIO.RISING, callback = self.switchpress, bouncetime = 100)
+#				GPIO.add_event_detect(INPUT_BUTTON_1_PIN, GPIO.RISING, callback = self.resetpress, bouncetime = 100)
+#
+#
+#		def get_buttons(self):
+#			buttons = []
+#			for n,b in self.buttons.items():
+#				i = GPIO.input(b)
+#				if i == BUTTON_PRESSED_STATE:
+#					buttons.append(True)
+#				else:
+#					buttons.append(False)
+#			return buttons
+#
+#		def get_buttonsDict(self):
+#			buttons = {}
+#			for n,b in self.buttons.items():
+#				i = GPIO.input(b)
+#				if i == BUTTON_PRESSED_STATE:
+#					buttons[n] = True
+#				else:
+#					buttons[n] = False
+#			return buttons
+#
+#		def led_state(self,led,state):
+#			if state:
+#				self.led_on(led)
+#			else:
+#				self.led_off(led)
+#
+#		def led_on(self,led):
+#			self.logger("led %s on" % led)
+#			GPIO.output(self.leds[led], LED_ON_STATE)
+#
+#		def led_off(self,led):
+#			self.logger("led %s on" % led)
+#			GPIO.output(self.leds[led], LED_OFF_STATE)
+#
+#		def relay_state(self,state):
+#			if state:
+#				self.relay_on(led)
+#			else:
+#				self.relay_off(led)
+#
+#		def relay_on(self):
+#			self.logger("relay on")
+#			GPIO.output(self.leds[led], RELAY_ON_STATE)
+#
+#		def relay_off(self):
+#			self.logger("relay off")
+#			GPIO.output(self.leds[led], RELAY_OFF_STATE)
+#
+#		def led_blink (self,led, duration=.05):
+#			GPIO.output(self.leds[led], LED_ON_STATE)
+#			time.sleep(duration)
+#			GPIO.output(self.leds[led], LED_OFF_STATE)
+#			time.sleep(duration)
+#except:
+#
+#	LED_OFF_STATE = 0
+#	LED_ON_STATE = 1
+#	RELAY_ON_STATE = 1
+#	RELAY_OFF_STATE = 0
+#	BUTTON_PRESSED_STATE =  0
+#	BUTTON_RELEASED_STATE = 1
+#
+#	class r00tsIoTGPIO():
+#		def __init__(self,  switchpress=lambda: None,resetpress=lambda: None,logfunc=lambda x: None):
+#			self.leds = LEDMAP
+#			self.buttons = BUTTONMAP
+#			self.logger = logfunc
+#			self.switchpress = switchpress
+#			self.resetpress = resetpress
+#
+#		def get_buttons(self):
+#				buttons = []
+#				for n,b in self.buttons.items():
+#					buttons.append(False)
+#				return buttons
+#
+#		def get_buttonsDict(self):
+#			buttons = {}
+#			for n,b in self.buttons.items():
+#				buttons[n] = False
+#			return buttons
+#
+#		def led_state(self,led,state):
+#			pass
+#
+#		def led_on(self,led):
+#			print("led %s on" % led)
+#			self.logger("led %s on" % led)
+#
+#		def led_off(self,led):
+#			print("led %s off" % led)
+#			self.logger("led %s off" % led)
+#
+#		def relay_on(self):
+#			print("relay on")
+#			self.logger("relay on")
+#
+#		def relay_off(self):
+#			print("relay off")
+#			self.logger("relay off")
+#
+#		def relay_state(self,state):
+#			pass
+#
+#		def led_blink(self,led, duration=.05):
+#			self.logger("led %s blink with duration %f" % (led,duration))
+#			print("led %s blink with duration %f" % (led,duration))
+
+
+
+class r00tsIoTGPIOBase():
+def __init__(self,  switchpress=lambda: None,resetpress=lambda: None,logfunc=lambda x: None):
+	self.leds = LEDMAP
+	self.buttons = BUTTONMAP
+	self.logger = logfunc
+	self.switchpress = switchpress
+	self.resetpress = resetpress
+
+def get_buttons(self):
+		buttons = []
+		for n,b in self.buttons.items():
+			buttons.append(False)
+		return buttons
+
+def get_buttonsDict(self):
+	buttons = {}
+	for n,b in self.buttons.items():
+		buttons[n] = False
+	return buttons
+
+def led_state(self,led,state):
+	pass
+	
+def led_on(self,led):
+	print("led %s on" % led)
+	self.logger("led %s on" % led)
+
+def led_off(self,led):
+	print("led %s off" % led)
+	self.logger("led %s off" % led)
+
+def relay_on(self):
+	print("relay on")
+	self.logger("relay on")
+
+def relay_off(self):
+	print("relay off")
+	self.logger("relay off")
+	
+def relay_state(self,state):
+	pass
+
+def led_blink(self,led, duration=.05):
+	self.logger("led %s blink with duration %f" % (led,duration))
+	print("led %s blink with duration %f" % (led,duration))
+
+
+
+class r00tsIoTRPiGPIO(r00tsIoTGPIOBase):
+	def __init__(self, switchpress=lambda: None,resetpress=lambda: None, logfunc=lambda: None):
+		self.leds = LEDMAP
+		self.buttons =BUTTONMAP
+		self.logger = logfunc
+		self.switchpress = switchpress
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+	
+		for n,l in self.leds.items():
+			GPIO.setup(l, GPIO.OUT)
+			
+		for n,b in self.buttons.items():
+			GPIO.setup(b, GPIO.IN)
+			
+		GPIO.setup(IOT_SWITCH_PIN, GPIO.OUT)
 		
-			for n,l in self.leds.items():
-				GPIO.setup(l, GPIO.OUT)
-				
-			for n,b in self.buttons.items():
-				GPIO.setup(b, GPIO.IN)
-				
-			GPIO.setup(IOT_SWITCH_PIN, GPIO.OUT)
-			
-			if BUTTON_PRESSED_STATE == 0:
-				GPIO.add_event_detect(INPUT_BUTTON_0_PIN, GPIO.FALLING, callback = self.switchpress, bouncetime = 100)
-				GPIO.add_event_detect(INPUT_BUTTON_1_PIN, GPIO.FALLING, callback = self.resetpress, bouncetime = 100)
+		if BUTTON_PRESSED_STATE == 0:
+			GPIO.add_event_detect(INPUT_BUTTON_0_PIN, GPIO.FALLING, callback = self.switchpress, bouncetime = 100)
+			GPIO.add_event_detect(INPUT_BUTTON_1_PIN, GPIO.FALLING, callback = self.resetpress, bouncetime = 100)
+		else:
+			GPIO.add_event_detect(INPUT_BUTTON_0_PIN, GPIO.RISING, callback = self.switchpress, bouncetime = 100)
+			GPIO.add_event_detect(INPUT_BUTTON_1_PIN, GPIO.RISING, callback = self.resetpress, bouncetime = 100)
+
+
+	def get_buttons(self):
+		buttons = []
+		for n,b in self.buttons.items():
+			i = GPIO.input(b)
+			if i == BUTTON_PRESSED_STATE:
+				buttons.append(True)
 			else:
-				GPIO.add_event_detect(INPUT_BUTTON_0_PIN, GPIO.RISING, callback = self.switchpress, bouncetime = 100)
-				GPIO.add_event_detect(INPUT_BUTTON_1_PIN, GPIO.RISING, callback = self.resetpress, bouncetime = 100)
+				buttons.append(False)
+		return buttons
 
-
-		def get_buttons(self):
-			buttons = []
-			for n,b in self.buttons.items():
-				i = GPIO.input(b)
-				if i == BUTTON_PRESSED_STATE:
-					buttons.append(True)
-				else:
-					buttons.append(False)
-			return buttons
-
-		def get_buttonsDict(self):
-			buttons = {}
-			for n,b in self.buttons.items():
-				i = GPIO.input(b)
-				if i == BUTTON_PRESSED_STATE:
-					buttons[n] = True
-				else:
-					buttons[n] = False
-			return buttons
-
-		def led_state(self,led,state):
-			if state:
-				self.led_on(led)
+	def get_buttonsDict(self):
+		buttons = {}
+		for n,b in self.buttons.items():
+			i = GPIO.input(b)
+			if i == BUTTON_PRESSED_STATE:
+				buttons[n] = True
 			else:
-				self.led_off(led)
-
-		def led_on(self,led):
-			self.logger("led %s on" % led)
-			GPIO.output(self.leds[led], LED_ON_STATE)
-
-		def led_off(self,led):
-			self.logger("led %s on" % led)
-			GPIO.output(self.leds[led], LED_OFF_STATE)
-
-		def relay_state(self,state):
-			if state:
-				self.relay_on(led)
-			else:
-				self.relay_off(led)
-				
-		def relay_on(self):
-			self.logger("relay on")
-			GPIO.output(self.leds[led], RELAY_ON_STATE)
-			
-		def relay_off(self):
-			self.logger("relay off")
-			GPIO.output(self.leds[led], RELAY_OFF_STATE)
-
-		def led_blink (self,led, duration=.05):
-			GPIO.output(self.leds[led], LED_ON_STATE)
-			time.sleep(duration)
-			GPIO.output(self.leds[led], LED_OFF_STATE)
-			time.sleep(duration)
-except:
-
-	LED_OFF_STATE = 0
-	LED_ON_STATE = 1
-	RELAY_ON_STATE = 1
-	RELAY_OFF_STATE = 0
-	BUTTON_PRESSED_STATE =  0
-	BUTTON_RELEASED_STATE = 1
-
-	class r00tsIoTGPIO():
-		def __init__(self,  switchpress=lambda: None,resetpress=lambda: None,logfunc=lambda x: None):
-			self.leds = LEDMAP
-			self.buttons = BUTTONMAP
-			self.logger = logfunc
-			self.switchpress = switchpress
-			self.resetpress = resetpress
-
-		def get_buttons(self):
-				buttons = []
-				for n,b in self.buttons.items():
-					buttons.append(False)
-				return buttons
-
-		def get_buttonsDict(self):
-			buttons = {}
-			for n,b in self.buttons.items():
 				buttons[n] = False
-			return buttons
+		return buttons
 
-		def led_state(self,led,state):
-			pass
+	def led_state(self,led,state):
+		if state:
+			self.led_on(led)
+		else:
+			self.led_off(led)
+
+	def led_on(self,led):
+		self.logger("led %s on" % led)
+		GPIO.output(self.leds[led], LED_ON_STATE)
+
+	def led_off(self,led):
+		self.logger("led %s on" % led)
+		GPIO.output(self.leds[led], LED_OFF_STATE)
+
+	def relay_state(self,state):
+		if state:
+			self.relay_on(led)
+		else:
+			self.relay_off(led)
 			
-		def led_on(self,led):
-			print("led %s on" % led)
-			self.logger("led %s on" % led)
+	def relay_on(self):
+		self.logger("relay on")
+		GPIO.output(self.leds[led], RELAY_ON_STATE)
+		
+	def relay_off(self):
+		self.logger("relay off")
+		GPIO.output(self.leds[led], RELAY_OFF_STATE)
 
-		def led_off(self,led):
-			print("led %s off" % led)
-			self.logger("led %s off" % led)
+	def led_blink (self,led, duration=.05):
+		GPIO.output(self.leds[led], LED_ON_STATE)
+		time.sleep(duration)
+		GPIO.output(self.leds[led], LED_OFF_STATE)
+		time.sleep(duration)
 
-		def relay_on(self):
-			print("relay on")
-			self.logger("relay on")
 
-		def relay_off(self):
-			print("relay off")
-			self.logger("relay off")
-			
-		def relay_state(self,state):
-			pass
 
-		def led_blink(self,led, duration=.05):
-			self.logger("led %s blink with duration %f" % (led,duration))
-			print("led %s blink with duration %f" % (led,duration))
+
+
+
+
 
 
 if __name__ == "__main__":
